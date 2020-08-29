ef941eff1c643ef43b7a8aecbe70f10808b88437
MischaTobias <mischa.tobias4b@gmail.com>
2020-08-29 09:42:51 -0600

    Initialized Lab1
    Created libraries project, console project and API

---
+++ IntegerTree/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace IntegerTree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
