0dee20b2cf519869ebc58c3cb830f089124cbf81
MischaTobias <mischa.tobias4b@gmail.com>
2020-09-05 19:13:24 -0600

    Create Movies.cs
    Review of where to save this class, pending.

---
+++ API/Models/Movies.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Models
+{
+    public class Movies
+    {
+        string Director { get; set; }
+        double ImdbRating { get; set; }
+        string Genre { get; set; }
+        string ReleaseDate { get; set; } //Verify the dataType
+        int RottenTomatoesRating { get; set; }
+        string Title { get; set; }
+    }
+}
